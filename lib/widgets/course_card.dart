326ff92d8f418f0963c0cef1b25faad20810a0b7
altrono <hbotema16@gmail.com>
2023-02-21 17:01:02 +0200

    created course card UI

---
+++ lib/widgets/course_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+class CourseCard extends StatelessWidget {
+  const CourseCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.onActionPressed,
+    required this.description,
+  }) : super(key: key);
+  final String image;
+  final String title;
+  final Function() onActionPressed;
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350.0,
+      child: Card(
+        child: InkWell(
+          onTap: onActionPressed,
+          child: Column(
+            children: [
+              Image.asset(
+                image,
+                height: 250,
+              ),
+              const SizedBox(height: 10.0),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              const SizedBox(height: 10.0),
+              Text(description)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
